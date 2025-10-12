86785718625855ac86c2facc01875dc17b787749
nayeong lee <lny021102@gmail.com>
2025-10-12 17:33:28 +0900

    design: favicon 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,11 @@ import { MockDataProvider } from "./contexts/MockDataContext";
 
 // 운영자 사이트는 네이버 등 검색 엔진에 등록하지 않습니다!
 export const metadata: Metadata = {
-  title: "TPT운영",
+  title: "TPT 운영",
   description: "트레이딩 피티 운영자 사이트입니다.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
